175ab91146d5daaf3ffcc704915b48b1b9aa53a8
kritzcreek <christoph@slamdata.com>
2017-08-28 15:12:16 +0200

    Validates that the specified home directory parses correctly

--- src/SlamData/AdminUI/MySettings/Component.purs
+++ src/SlamData/AdminUI/MySettings/Component.purs
@@ -14,11 +14,12 @@ See the License for the specific language governing permissions and
 limitations under the License.
 -}
 
-module SlamData.AdminUI.MySettings.Component where
+module SlamData.AdminUI.MySettings.Component (component, Query(..)) where
 
 import SlamData.Prelude
 
 import Data.Argonaut as J
+import Data.Path.Pathy (parseAbsDir)
 import Halogen as H
 import Halogen.HTML as HH
 import Halogen.HTML.Events as HE
@@ -27,6 +28,7 @@ import SlamData.LocalStorage.Class as LS
 import SlamData.LocalStorage.Keys as LK
 import SlamData.Monad (Slam)
 import SlamData.Theme.Theme as Theme
+import Utils.Path (sandbox)
 
 data Query a
   = Init a
@@ -35,12 +37,14 @@ data Query a
 
 type State =
   { homeDirectory ∷ String
+  , homeDirectoryError ∷ Maybe String
   , defaultTheme ∷ String
   }
 
 defaultState ∷ State
 defaultState =
   { homeDirectory: "/"
+  , homeDirectoryError: Nothing
   , defaultTheme: "Dark"
   }
 
@@ -87,8 +91,13 @@ component =
               _→ Theme.default
           pure next
         HomeDirectoryChanged newDirectory next → do
-          LS.persist J.encodeJson LK.adminUIHomeDirectory newDirectory
-          H.modify (_ { homeDirectory = newDirectory })
+          err ← validateHomeDirectory newDirectory
+          case err of
+            Just msg →
+              H.modify ( _ { homeDirectoryError = Just msg })
+            Nothing → do
+              LS.persist J.encodeJson LK.adminUIHomeDirectory newDirectory
+              H.modify (_ { homeDirectory = newDirectory, homeDirectoryError = Nothing })
           pure next
 
 themes ∷ Array String
@@ -96,25 +105,34 @@ themes = ["Dark", "Light"]
 
 renderForm ∷ State → Array HTML
 renderForm state =
-    [ HH.fieldset
-        [ HP.class_ (HH.ClassName "home-directory") ]
-        [ HH.legend_ [ HH.text "Location of my home directory in the SlamData file system:" ]
-        , HH.input
-            [ HP.classes [ HH.ClassName "form-control" ]
-            , HP.id_ "HomeDirectory"
-            , HP.value state.homeDirectory
-            , HE.onValueChange (HE.input HomeDirectoryChanged)
-            ]
-        ]
-    , HH.fieldset
-        [ HP.class_ (HH.ClassName "themes") ]
-        [ HH.legend_ [ HH.text "Default theme for new decks:" ]
-        , HH.select
-            [ HP.classes [ HH.ClassName "form-control" ]
-            , HP.id_ "ThemeSelection"
-            , HE.onValueChange (HE.input DefaultThemeChanged)
-            , HP.value state.defaultTheme
-            ]
-            (themes <#> \t → HH.option_ [HH.text t])
-        ]
-    ]
+  [ HH.fieldset
+      [ HP.class_ (HH.ClassName "home-directory") ]
+      [ HH.legend_ [ HH.text "Location of my home directory in the SlamData file system:" ]
+      , HH.input
+          [ HP.classes [ HH.ClassName "form-control" ]
+          , HP.id_ "HomeDirectory"
+          , HP.value state.homeDirectory
+          , HE.onValueChange (HE.input HomeDirectoryChanged)
+          ]
+      ]
+  , HH.fieldset
+      [ HP.class_ (HH.ClassName "themes") ]
+      [ HH.legend_ [ HH.text "Default theme for new decks:" ]
+      , HH.select
+          [ HP.classes [ HH.ClassName "form-control" ]
+          , HP.id_ "ThemeSelection"
+          , HE.onValueChange (HE.input DefaultThemeChanged)
+          , HP.value state.defaultTheme
+          ]
+          (themes <#> \t → HH.option_ [HH.text t])
+      ]
+  ]
+
+-- | Checks whether the input parses as a valid directory
+validateHomeDirectory ∷ String → DSL (Maybe String)
+validateHomeDirectory s =
+  case parseAbsDir s >>= sandbox of
+    Nothing →
+      pure (Just "Please enter a directory path.")
+    Just dir → do
+      pure Nothing
